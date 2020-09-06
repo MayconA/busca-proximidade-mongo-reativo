b8cd0e1993ff03ded07277952ae2ea5f0f4fc3a4
Maycon Andrade <maycon_andrade@yahoo.com.br>
2020-09-06 00:33:42 -0300

    feat:Adicionar BlockHound para detectar chamada bloqueantes

--- src/main/java/com/maa/buscaproximidadereativo/BuscaProximidadeReativoApplication.java
+++ src/main/java/com/maa/buscaproximidadereativo/BuscaProximidadeReativoApplication.java
@@ -3,11 +3,15 @@ package com.maa.buscaproximidadereativo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.blockhound.BlockHound;
+
 @SpringBootApplication
 public class BuscaProximidadeReativoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuscaProximidadeReativoApplication.class, args);
+		
+		BlockHound.install();
 	}
 
 }
